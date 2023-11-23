ed2480954615a31b250abc5cd593841fdfabf6a2
JitHoon <imessi0336@gmail.com>
2023-11-23 18:09:02 +0900

    Design: 예약하기 페이지 스타일 적용

---
+++ src/feature/reservation/styles/guestInformation.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const GuestInfoTitle = styled.h2`
+  margin-bottom: 15px;
+
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+export const GuestName = styled.span`
+  padding: 5px;
+`;
+
+export const GuestEmail = styled.span`
+  padding: 5px;
+`;
