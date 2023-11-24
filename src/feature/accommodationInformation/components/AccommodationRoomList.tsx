4dd5d82369c5df61d0ae81aeb78f9997c1b798d6
xxxjinn <tlsguswls319@naver.com>
2023-11-24 14:00:20 +0900

    Feat: 숙소 정보/룸 상세 정보 api 연결, skeleton 적용

--- src/feature/accommodationInformation/components/AccommodationRoomList.tsx
+++ src/feature/accommodationInformation/components/AccommodationRoomList.tsx
@@ -1,67 +1,72 @@
+import { useQuery } from "@tanstack/react-query";
 import { Wrapper } from "../styles/accommodationRoomList";
 import AccommodationRoomItem from "./AccommodationRoomItem";
-
-/**나중에 실제 데이터 사용 */
-const mockAccommodation = [
-  {
-    id: 1,
-    name: "초특급대박대박슈퍼최고짱완전좋은방",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 2,
-    name: "룸2",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 3,
-    name: "룸3",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 4,
-    name: "룸4",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 5,
-    name: "룸5",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-];
+import { useParams } from "react-router-dom";
+import instance from "../../../api/instance";
+import * as style from "../styles/accommodationRoomItem";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const AccommodationRoomList = () => {
+  const { id } = useParams();
+
+  {
+    /* 추후 분리 */
+  }
+  const { status, data, error } = useQuery({
+    queryKey: ["getAccommodationInfoData"],
+    queryFn: async () => {
+      const startDate = "2023-11-21";
+      const endDate = "2023-11-22";
+      const guest = 2;
+
+      const { data } = await instance.get(`/accommodations/${id}`, {
+        params: {
+          startDate,
+          endDate,
+          guest,
+          accommodationId: id,
+        },
+      });
+      return data;
+    },
+  });
+
+  if (status === "pending") {
+    return (
+      <Wrapper>
+        {[...Array(4)].map((_, index) => (
+          <style.Box key={index}>
+            <style.RoomImgWrapper>
+              <Skeleton width={300} height={150} />
+            </style.RoomImgWrapper>
+            <style.SkeletonRoomInfo>
+              <style.SkeletonRoomTopWrapper>
+                <Skeleton height={30} width={180} />
+                <Skeleton width={280} height={20} />
+              </style.SkeletonRoomTopWrapper>
+              <Skeleton height={25} width={170} />
+            </style.SkeletonRoomInfo>
+          </style.Box>
+        ))}
+      </Wrapper>
+    );
+  } else if (status === "error") {
+    console.log(error.message, ": 알 수 없는 오류입니다.");
+  }
+
   return (
     <Wrapper>
-      {mockAccommodation.map(v => (
+      {data.data.roomOptions.map(room => (
         <AccommodationRoomItem
-          key={v.id}
-          id={v.id}
-          name={v.name}
-          img={v.img}
-          checkIn={v.checkIn}
-          checkOut={v.checkOut}
-          price={v.price}
+          key={room.id}
+          id={room.id}
+          accommodationId={id}
+          name={room.name}
+          img={room.roomOptionImage.mainImageUrls[0]}
+          checkIn={room.checkInTime}
+          checkOut={room.checkOutTime}
+          price={room.price}
         />
       ))}
     </Wrapper>
