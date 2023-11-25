213e5490ac89cecaee64434f1705b39c2d2f649e
xxxjinn <tlsguswls319@naver.com>
2023-11-25 17:53:44 +0900

    Feat: 방 상세 페이지 api 연결에 따른 bottom bar 수정

--- src/feature/accommodationRoom/components/BottomBar.tsx
+++ src/feature/accommodationRoom/components/BottomBar.tsx
@@ -1,25 +1,41 @@
 import { useRecoilState } from "recoil";
 import { toastState } from "../../../recoil/toast";
 import * as style from "../styles/bottomBar";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
 
-const BottomBar = () => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const BottomBar = ({ status, data, error }) => {
   const [, setToast] = useRecoilState(toastState);
 
-  return (
-    <style.Wrapper>
-      {/* 가격 하드코딩 */}
-      <style.RoomPrice>250,000 원</style.RoomPrice>
-      <style.ButtonWrapper>
-        <style.CartButton onClick={() => setToast({ open: true, message: "장바구니에 상품이 담겼습니다." })}>
-          <style.CartIcon />
-        </style.CartButton>
-        <Link to="/reservation">
-          <style.ReservationButton>예약하기</style.ReservationButton>
-        </Link>
-      </style.ButtonWrapper>
-    </style.Wrapper>
-  );
+  const { id } = useParams<Record<string, string | undefined>>();
+
+  if (status == "pending") {
+    return (
+      <style.Wrapper>
+        <Skeleton height={30} width={200} />
+      </style.Wrapper>
+    );
+  } else if (status == "error") {
+    console.log(error.message);
+  } else {
+    const room = data.data.roomOptions.find(option => option.id === parseInt(id as string));
+    return (
+      <style.Wrapper>
+        <style.RoomPrice>
+          {room.totalPrice.toLocaleString()} 원 / {room.stayDuration}박
+        </style.RoomPrice>
+        <style.ButtonWrapper>
+          <style.CartButton onClick={() => setToast({ open: true, message: "장바구니에 상품이 담겼습니다." })}>
+            <style.CartIcon />
+          </style.CartButton>
+          <Link to="/reservation">
+            <style.ReservationButton>예약하기</style.ReservationButton>
+          </Link>
+        </style.ButtonWrapper>
+      </style.Wrapper>
+    );
+  }
 };
 
 export default BottomBar;
