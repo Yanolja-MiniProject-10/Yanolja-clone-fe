e513bec1c659b2113fec0330eac8b3debea00469
xxxjinn <tlsguswls319@naver.com>
2023-11-25 17:51:55 +0900

    Feat: 방 상세 소개 페이지 api 연결

--- src/feature/accommodationRoom/components/AccommodationRoomInfo.tsx
+++ src/feature/accommodationRoom/components/AccommodationRoomInfo.tsx
@@ -3,132 +3,83 @@ import Toast from "../../../components/Toast/Toast";
 import { useRecoilState } from "recoil";
 import { toastState } from "../../../recoil/toast";
 import * as style from "../styles/accommodationRoomInfo";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Skeleton from "react-loading-skeleton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
 
-/**나중에 실제 데이터 사용 */
-const mockAccommodation = [
-  {
-    id: 1,
-    name: "초특급대박대박슈퍼최고짱완전좋은방",
-    description: "이 호텔은 오래되었습니다.",
-    img: [
-      "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-      "https://yaimg.yanolja.com/v5/2022/10/20/13/1280/63514b13bb9288.13141700.jpg",
-    ],
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 2,
-    name: "룸2",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 3,
-    name: "룸3",
-    description: "이 호텔은 짱 많이 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 4,
-    name: "룸4",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-  {
-    id: 5,
-    name: "룸5",
-    description: "이 호텔은 오래되었습니다.",
-    img: "https://yaimg.yanolja.com/v5/2023/11/01/12/1280/65423f2b0e6d61.93360681.jpg",
-    checkIn: "15:00",
-    checkOut: "11:00",
-    price: 250000,
-  },
-];
-
-const AccommodationRoomInfo = () => {
+const AccommodationRoomInfo = ({ status, data, error }) => {
   const [toast, setToast] = useRecoilState(toastState);
+  const { id } = useParams<Record<string, string | undefined>>();
 
-  /**url 데이터 받아올 때 수정 */
-  const { id } = useParams();
-  const room = mockAccommodation.find(v => v.id == parseInt(id as string));
+  if (status == "pending") {
+    return (
+      <style.Wrapper>
+        <Skeleton height={560} />
+        <style.TextInfo>
+          <Skeleton height={30} width={200} />
+          <Skeleton width={300} height={25} />
+          <style.DivideLine />
+          <style.StaticDesc>상세 소개</style.StaticDesc>
+          <Skeleton />
+          <Skeleton />
+          <Skeleton width={200} />
+        </style.TextInfo>
+      </style.Wrapper>
+    );
+  } else if (status == "error") {
+    console.log(error.message);
+  } else {
+    const room = data.data.roomOptions.find(option => option.id === parseInt(id as string));
 
-  {
-    /** 나중에 room 타입 확실히 지정한 후 삭제 예정 */
-  }
-  if (!room) {
-    return <div>Room not found</div>;
-  }
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "0px",
-    draggable: false,
-    initialSlide: 0,
-    prevArrow: <style.CustomPrevArrow />,
-    nextArrow: <style.CustomNextArrow />,
-    responsive: [
-      // 반응형 옵션
-      {
-        breakpoint: 840, // (숫자)px 이하일 경우
-        settings: {
-          slidesToShow: 1,
-          arrows: true,
-        },
-      },
-    ],
-  };
-
-  return (
-    <style.Wrapper>
-      <Slider {...settings}>
-        {Array.isArray(room.img) ? (
-          room.img.map((img, index) => <style.RoomImg key={index} src={img} alt={room.name} />)
+    return (
+      <style.Wrapper>
+        {room.roomOptionImage.mainImageUrls.length > 1 ? (
+          <Swiper
+            slidesPerView={1}
+            navigation={true}
+            loop={true}
+            modules={[Navigation, Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {room.roomOptionImage.mainImageUrls.map((img, index) => (
+              <SwiperSlide key={index}>
+                <style.RoomImg src={img} alt={room.name} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         ) : (
           <style.RoomImg src={room.img} alt={room.name} />
         )}
-      </Slider>
-      <style.TextInfo>
-        <style.RoomName>{room.name}</style.RoomName>
-        <style.RoomCheckInOut>
-          <style.RoomCheckIn>
-            <span>체크인: </span>
-            {room.checkIn}
-          </style.RoomCheckIn>
-          <span>~</span>
-          <style.RoomCheckOut>
-            <span>체크아웃: </span>
-            {room.checkOut}
-          </style.RoomCheckOut>
-        </style.RoomCheckInOut>
-        <style.RoomPrice> {room.price} 원</style.RoomPrice>
-        <style.DivideLine />
-        <style.RoomStaticDescWrap>
-          <style.StaticDesc>상세 소개</style.StaticDesc>
-          <style.RoomDesc>{room.description}</style.RoomDesc>
-        </style.RoomStaticDescWrap>
-      </style.TextInfo>
-      {toast.open && <Toast setToast={setToast} />}
-    </style.Wrapper>
-  );
+        <style.TextInfo>
+          <style.RoomName>{room.name}</style.RoomName>
+          <style.RoomCheckInOut>
+            <style.RoomCheckIn>
+              <span>체크인: </span>
+              {room.checkInTime.slice(0, 5)}
+            </style.RoomCheckIn>
+            <span>~</span>
+            <style.RoomCheckOut>
+              <span>체크아웃: </span>
+              {room.checkOutTime.slice(0, 5)}
+            </style.RoomCheckOut>
+          </style.RoomCheckInOut>
+          <style.RoomPrice>
+            {" "}
+            {room.totalPrice.toLocaleString()} 원 / {room.stayDuration}박
+          </style.RoomPrice>
+          <style.DivideLine />
+          <style.RoomStaticDescWrap>
+            <style.StaticDesc>상세 소개</style.StaticDesc>
+            <style.RoomDesc>{room.description}</style.RoomDesc>
+          </style.RoomStaticDescWrap>
+        </style.TextInfo>
+        {toast.open && <Toast setToast={setToast} />}
+      </style.Wrapper>
+    );
+  }
 };
 
 export default AccommodationRoomInfo;
