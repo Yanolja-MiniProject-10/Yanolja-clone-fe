e5e5959eee1db45ac255a9713e77231cc17301d6
JitHoon <imessi0336@gmail.com>
2023-11-26 18:48:14 +0900

    Fix: styled-componrnt 컨벤션 적용

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,20 +1,20 @@
 import SelectCartList from "../feature/cart/components/SelectCartList";
 import TotalSelectedPrice from "../feature/cart/components/TotalSelectedPrice";
 import CartPay from "../feature/cart/components/CartPay";
-import { useFetchCarts } from "../feature/cart/cart.queries";
-import { CartWrapper } from "../styles/cart";
+import { useFetchCarts } from "../feature/cart/hooks/queries/useFetchCarts";
+import * as style from "../styles/cart";
 
 const Cart = () => {
-  const { data, isLoading, isError } = useFetchCarts();
+  const { data: cartData, isLoading, isError } = useFetchCarts();
 
   isLoading ? console.log("로딩중") : console.log("로딩끝");
 
   return (
-    <CartWrapper>
-      <SelectCartList accomodations={isError ? null : data?.accommodations} />
+    <style.CartWrapper>
+      <SelectCartList accomodations={isError ? null : cartData?.accommodations} />
       <TotalSelectedPrice />
       <CartPay />
-    </CartWrapper>
+    </style.CartWrapper>
   );
 };
 
