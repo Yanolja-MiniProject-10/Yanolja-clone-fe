67b50096ce202e2fc539aba3abff2dec5d346182
applevalley <skatkd7571@naver.com>
2023-11-27 14:56:00 +0900

    feat: 카테고리 페이지 생성 및 App.tsx 연결

---
+++ src/pages/Category.tsx
@@ -0,0 +1,14 @@
+// import AccommodationContent from "../components/accommodation/AccommodationContent";
+import AccommodationNav from "../components/accommodation/AccommodationNav";
+import { AccommodationLayout } from "../feature/accommodation/styles/accommodationLayout";
+
+const Category = () => {
+  return (
+    <AccommodationLayout>
+      <AccommodationNav />
+      {/* <AccommodationContent /> */}
+    </AccommodationLayout>
+  );
+};
+
+export default Category;
