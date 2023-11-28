85e072550cd8f5a1f33cda8d11288c0f6dd5e987
applevalley <skatkd7571@naver.com>
2023-11-28 15:22:09 +0900

    feat: 엔터키 입력해서 상품 조회해오게 변경

--- src/feature/search/components/AccommmodationSearchResult.tsx
+++ src/feature/search/components/AccommmodationSearchResult.tsx
@@ -3,7 +3,7 @@ import { useRecoilValue } from "recoil";
 import { accommodationDateState } from "../../../recoil/accommodation/accommodationDate";
 import { accommodationMemberState } from "../../../recoil/accommodation/accommodationMember";
 import { useAccommodationsSearchQuery } from "../hooks/search.hooks";
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import { AccommodationSetSearchResultParams } from "../search.types";
 
 const AccommmodationSearchResult = ({ setAccommodations }: AccommodationSetSearchResultParams) => {
@@ -18,9 +18,13 @@ const AccommmodationSearchResult = ({ setAccommodations }: AccommodationSetSearc
     name: inputValue,
   });
 
-  if (data?.data.content && status === "success") {
-    setAccommodations(data.data.content);
-  }
+  const handleEnterpress = async (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (inputValue && data?.data.content && status === "success") {
+        setAccommodations(data.data.content);
+      }
+    }
+  };
 
   return (
     <style.AccommodationSearchInputBox>
@@ -29,7 +33,7 @@ const AccommmodationSearchResult = ({ setAccommodations }: AccommodationSetSearc
         placeholder="숙소를 검색하세요"
         onChange={e => setInputValue(e.target.value)}
         value={inputValue}
-        // onKeyUp={handleEnterpress}
+        onKeyUp={handleEnterpress}
       />
     </style.AccommodationSearchInputBox>
   );
