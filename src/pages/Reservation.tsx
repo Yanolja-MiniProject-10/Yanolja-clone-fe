ea7039841a67cf38e2e561548825035549eba5c8
JitHoon <imessi0336@gmail.com>
2023-11-28 21:23:52 +0900

    Feat: 예약 화면 입장 시 데이터 결제 대기 데이터 요청 및 라디오 인풋 초기화

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,9 +1,39 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { paymentDataState } from "../recoil/paymentData";
+import { radioDataState } from "../recoil/checkedRadio";
+import { usePostPaymentCart } from "../feature/reservation/hooks/queries/useFetchPaymentCart";
 import ReservationSummary from "../feature/reservation/components/ReservationSummary";
 import GuestInformation from "../feature/reservation/components/GuestInformation";
 import ReservationPay from "../feature/reservation/components/ReservationPay";
+import { PostPaymentCartPayload } from "../feature/reservation/reservation.types";
 import { ReservationWrapper } from "../styles/reservation";
 
 const Reservation = () => {
+  const location = useLocation();
+  const postPaymentCartPayload: PostPaymentCartPayload = { ...location.state };
+  const { mutateAsync: postPaymentCart } = usePostPaymentCart();
+  const [paymentData, setPaymentData] = useRecoilState(paymentDataState);
+  const setRadioData = useSetRecoilState(radioDataState);
+
+  useEffect(() => {
+    const handlePaymentCart = async () => {
+      try {
+        await postPaymentCart({
+          postPaymentCartPayload,
+          paymentData,
+          setPaymentData,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    handlePaymentCart();
+    setRadioData({});
+  }, []);
+
   return (
     <ReservationWrapper>
       <ReservationSummary />
