3783d5dfc12d6479290603943e5fa50ceefac4fd
JitHoon <imessi0336@gmail.com>
2023-11-29 14:52:51 +0900

    Feat: 구매 완료 POST API 적용

--- src/feature/reservation/components/ReservationPayBtn.tsx
+++ src/feature/reservation/components/ReservationPayBtn.tsx
@@ -1,31 +1,43 @@
 import { useRecoilValue } from "recoil";
 import { radioDataState } from "../../../recoil/checkedRadio";
-import { useLocation } from "react-router-dom";
-import { RadioDataState } from "../reservation.types";
+import { useLocation, useNavigate } from "react-router-dom";
+import { RadioDataState, ReservationLocationsState } from "../reservation.types";
 import { PayBtn } from "../styles/reservationPayBtn";
+import { usePostPurchase } from "../hooks/queries/usePostPurchase";
 
 const ReservationPayBtn = ({ allChecked }: { allChecked: boolean }) => {
   const radioData = useRecoilValue(radioDataState);
   const radioDataArray: RadioDataState[] = Object.values(radioData);
   const location = useLocation();
-  const userInfo = { ...location.state };
 
-  console.log(radioDataArray);
+  const ReservationInfo: ReservationLocationsState = { ...location.state };
+  const { mutateAsync: postPurchase } = usePostPurchase();
+
+  const navigation = useNavigate();
+
+  const postPurchasePayload = {
+    cartId: ReservationInfo.cartId,
+    cartProducts: radioDataArray,
+  };
+
+  const handlePurchase = async () => {
+    try {
+      const data = await postPurchase(postPurchasePayload);
+
+      if (data === "SUCCESS") {
+        navigation("/reservation-list");
+      } else {
+        throw new Error();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <PayBtn
-      $allChecked={allChecked && userInfo.cartProducts.length === radioDataArray.length}
-      onClick={
-        allChecked && userInfo.cartProducts.length === radioDataArray.length
-          ? () => {
-              console.log("결제하기 API");
-              console.log({
-                cartId: userInfo.cartId,
-                cartProducts: radioDataArray,
-              });
-            }
-          : undefined
-      }
+      $allChecked={allChecked && ReservationInfo.cartProducts.length === radioDataArray.length}
+      onClick={allChecked && ReservationInfo.cartProducts.length === radioDataArray.length ? handlePurchase : undefined}
     >
       결제하기
     </PayBtn>
