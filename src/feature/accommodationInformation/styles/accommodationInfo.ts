72b718409490b62fbc32d04bc677c49af6b2be6c
xxxjinn <tlsguswls319@naver.com>
2023-11-29 15:47:40 +0900

    Design: img 수치 변경

--- src/feature/accommodationInformation/styles/accommodationInfo.ts
+++ src/feature/accommodationInformation/styles/accommodationInfo.ts
@@ -15,7 +15,7 @@ export const SkeletonImgWrapper = styled.div`
 
 export const ImgWrapper = styled.div`
   width: 100%;
-  height: 31rem;
+  height: 500px;
 `;
 
 export const AccomodationImg = styled.img`
