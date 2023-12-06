ca2c247e9b565c7b68989aa0d9446e5efbfba4e1
xxxjinn <tlsguswls319@naver.com>
2023-12-07 02:25:08 +0900

    Refactor: toast가 사라진 후 장바구니에 담는 것을 허용

--- src/feature/accommodationInformation/components/CartButton.tsx
+++ src/feature/accommodationInformation/components/CartButton.tsx
@@ -7,6 +7,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { usePostCart } from "../hooks/queries/addCartData";
 import { CartButtonProps } from "../accommodationInformation.types";
 import { loginModalState } from "../recoil/accommodationLoginModal";
+import { useState } from "react";
 
 const CartButton = ({
   available,
@@ -27,8 +28,11 @@ const CartButton = ({
 
   const setLogInModal = useSetRecoilState(loginModalState);
 
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
   const handleAddCart = async () => {
     try {
+      setIsButtonDisabled(true);
       await postCart({
         roomOptionId,
         numberOfGuest,
@@ -40,11 +44,13 @@ const CartButton = ({
     } catch (e) {
       window.alert("사용 중 문제가 발생했습니다. 메인에서 다시 시도해주세요.");
       navigation("/");
+    } finally {
+      setTimeout(() => setIsButtonDisabled(false), 3000);
     }
   };
 
   return available ? (
-    <style.CartButton onClick={() => (user.accessToken ? handleAddCart() : setLogInModal(true))}>
+    <style.CartButton onClick={() => !isButtonDisabled && (user.accessToken ? handleAddCart() : setLogInModal(true))}>
       <style.CartIcon />
     </style.CartButton>
   ) : (
