c218cdeeff6a4e391d3b26659d95e02680295e90
xxxjinn <tlsguswls319@naver.com>
2023-12-07 10:50:10 +0900

    Refactor: 형태 수정

--- src/feature/accommodationInformation/components/AccommodationRoomItem.tsx
+++ src/feature/accommodationInformation/components/AccommodationRoomItem.tsx
@@ -48,107 +48,105 @@ const AccommodationRoomItem = ({
   }
 
   return (
-    <>
-      <style.Box>
-        <style.RoomImgWrapper>
-          {roomOptionImage.mainImageUrls.length > 1 ? (
-            <Swiper
-              style={{ zIndex: 0 }}
-              slidesPerView={1}
-              navigation={true}
-              loop={true}
-              modules={[Navigation, Pagination]}
-              pagination={{ clickable: true }}
-            >
-              {roomOptionImage.mainImageUrls.map((img, index) => (
-                <SwiperSlide key={index}>
-                  <style.RoomImg src={img} alt={name} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+    <style.Box>
+      <style.RoomImgWrapper>
+        {roomOptionImage.mainImageUrls.length > 1 ? (
+          <Swiper
+            style={{ zIndex: 0 }}
+            slidesPerView={1}
+            navigation={true}
+            loop={true}
+            modules={[Navigation, Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {roomOptionImage.mainImageUrls.map((img, index) => (
+              <SwiperSlide key={index}>
+                <style.RoomImg src={img} alt={name} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        ) : (
+          <style.RoomImg src={roomOptionImage.mainImageUrls[0]} alt={name} />
+        )}
+      </style.RoomImgWrapper>
+      <style.RoomInfo>
+        <style.RoomTopWrapper>
+          <style.RoomName>{name.length >= 16 ? name.slice(0, 15) + "..." : name}</style.RoomName>
+          <Link to={`/accommodation/${accommodationId}/room/${id}`}>
+            <style.RoomDetailButton>상세보기 {">"}</style.RoomDetailButton>
+          </Link>
+        </style.RoomTopWrapper>
+        <style.RoomCheckInOut>
+          체크인: {checkInTime.slice(0, 5)} ~ 체크아웃: {checkOutTime.slice(0, 5)}
+        </style.RoomCheckInOut>
+        <style.RoomPrice>
+          {totalPrice.toLocaleString()}원 / {stayDuration}박
+        </style.RoomPrice>
+        {isAvailableGuest ? (
+          <style.CapacityWrapper>
+            <style.GuestIcon />
+            <style.GuestNumber>인원: {capacity}인</style.GuestNumber>
+          </style.CapacityWrapper>
+        ) : (
+          <style.CapacityWrapper>
+            <style.GuestIcon />
+            <style.GuestNumber>인원: {capacity}인</style.GuestNumber>
+            <style.NoAvailableRoom>* 선택하신 인원으로 이용이 불가능합니다.</style.NoAvailableRoom>
+          </style.CapacityWrapper>
+        )}
+        <style.BottomWrapper>
+          {isAvailableDate ? (
+            <style.RoomCount>남은 객실 수: {availableRoomCount}개</style.RoomCount>
           ) : (
-            <style.RoomImg src={roomOptionImage.mainImageUrls[0]} alt={name} />
+            <style.NoAvailableRoom>* 예약이 마감되었습니다.</style.NoAvailableRoom>
           )}
-        </style.RoomImgWrapper>
-        <style.RoomInfo>
-          <style.RoomTopWrapper>
-            <style.RoomName>{name.length >= 16 ? name.slice(0, 15) + "..." : name}</style.RoomName>
-            <Link to={`/accommodation/${accommodationId}/room/${id}`}>
-              <style.RoomDetailButton>상세보기 {">"}</style.RoomDetailButton>
-            </Link>
-          </style.RoomTopWrapper>
-          <style.RoomCheckInOut>
-            체크인: {checkInTime.slice(0, 5)} ~ 체크아웃: {checkOutTime.slice(0, 5)}
-          </style.RoomCheckInOut>
-          <style.RoomPrice>
-            {totalPrice.toLocaleString()}원 / {stayDuration}박
-          </style.RoomPrice>
-          {isAvailableGuest ? (
-            <style.CapacityWrapper>
-              <style.GuestIcon />
-              <style.GuestNumber>인원: {capacity}인</style.GuestNumber>
-            </style.CapacityWrapper>
-          ) : (
-            <style.CapacityWrapper>
-              <style.GuestIcon />
-              <style.GuestNumber>인원: {capacity}인</style.GuestNumber>
-              <style.NoAvailableRoom>* 선택하신 인원으로 이용이 불가능합니다.</style.NoAvailableRoom>
-            </style.CapacityWrapper>
-          )}
-          <style.BottomWrapper>
-            {isAvailableDate ? (
-              <style.RoomCount>남은 객실 수: {availableRoomCount}개</style.RoomCount>
+          <style.ButtonWrapper>
+            {isAvailableDate && isAvailableGuest ? (
+              <>
+                <CartButton
+                  roomOptionId={id}
+                  numberOfGuest={guest}
+                  reservationStartDate={reservationStartDate}
+                  reservationEndDate={reservationEndDate}
+                  stayDuration={stayDuration}
+                  available={true}
+                />
+                <ReservationButton
+                  id={id}
+                  guest={capacity}
+                  reservationStartDate={reservationStartDate}
+                  reservationEndDate={reservationEndDate}
+                  stayDuration={stayDuration}
+                  available={true}
+                  text="예약하기"
+                />
+              </>
             ) : (
-              <style.NoAvailableRoom>* 예약이 마감되었습니다.</style.NoAvailableRoom>
+              <>
+                <CartButton
+                  roomOptionId={id}
+                  numberOfGuest={guest}
+                  reservationStartDate={reservationStartDate}
+                  reservationEndDate={reservationEndDate}
+                  stayDuration={stayDuration}
+                  available={false}
+                />
+                <ReservationButton
+                  available={false}
+                  id={id}
+                  guest={capacity}
+                  reservationStartDate={reservationStartDate}
+                  reservationEndDate={reservationEndDate}
+                  stayDuration={stayDuration}
+                  text="예약불가"
+                />
+              </>
             )}
-            <style.ButtonWrapper>
-              {isAvailableDate && isAvailableGuest ? (
-                <>
-                  <CartButton
-                    roomOptionId={id}
-                    numberOfGuest={guest}
-                    reservationStartDate={reservationStartDate}
-                    reservationEndDate={reservationEndDate}
-                    stayDuration={stayDuration}
-                    available={true}
-                  />
-                  <ReservationButton
-                    id={id}
-                    guest={capacity}
-                    reservationStartDate={reservationStartDate}
-                    reservationEndDate={reservationEndDate}
-                    stayDuration={stayDuration}
-                    available={true}
-                    text="예약하기"
-                  />
-                </>
-              ) : (
-                <>
-                  <CartButton
-                    roomOptionId={id}
-                    numberOfGuest={guest}
-                    reservationStartDate={reservationStartDate}
-                    reservationEndDate={reservationEndDate}
-                    stayDuration={stayDuration}
-                    available={false}
-                  />
-                  <ReservationButton
-                    available={false}
-                    id={id}
-                    guest={capacity}
-                    reservationStartDate={reservationStartDate}
-                    reservationEndDate={reservationEndDate}
-                    stayDuration={stayDuration}
-                    text="예약불가"
-                  />
-                </>
-              )}
-            </style.ButtonWrapper>
-          </style.BottomWrapper>
-        </style.RoomInfo>
-      </style.Box>
+          </style.ButtonWrapper>
+        </style.BottomWrapper>
+      </style.RoomInfo>
       {logInModal && <LoginModal onClose={() => setLogInModal(false)} />}
-    </>
+    </style.Box>
   );
 };
 
--- src/feature/accommodationRoom/components/BottomBar.tsx
+++ src/feature/accommodationRoom/components/BottomBar.tsx
@@ -48,66 +48,63 @@ const BottomBar = ({ status, data }: RoomInfoProps) => {
     const isAvailableGuest = guest <= room.capacity;
 
     return (
-      <>
-        <style.Wrapper>
-          <style.TopWrapper>
-            <style.ReservationDate>
-              {reservationStartDate} ~ {reservationEndDate}
-            </style.ReservationDate>
-            <style.ReservationGuest> 선택 인원: {guest}인</style.ReservationGuest>
-            <style.RoomPrice>
-              {room.totalPrice.toLocaleString()}원 / {room.stayDuration}박
-            </style.RoomPrice>
-          </style.TopWrapper>
-          <style.ButtonWrapper>
-            {isRoomAvailable && isAvailableGuest ? (
-              <>
-                <CartButton
-                  roomOptionId={room.id}
-                  numberOfGuest={guest}
-                  reservationStartDate={reservationStartDate}
-                  reservationEndDate={reservationEndDate}
-                  stayDuration={room.stayDuration}
-                  available={true}
-                />
-                <ReservationButton
-                  id={room.id}
-                  guest={guest}
-                  reservationStartDate={reservationStartDate}
-                  reservationEndDate={reservationEndDate}
-                  stayDuration={room.stayDuration}
-                  available={true}
-                  text="예약하기"
-                  $isWide={true}
-                />
-              </>
-            ) : (
-              <>
-                <CartButton
-                  roomOptionId={room.id}
-                  numberOfGuest={guest}
-                  reservationStartDate={reservationStartDate}
-                  reservationEndDate={reservationEndDate}
-                  stayDuration={room.stayDuration}
-                  available={false}
-                />
-                <ReservationButton
-                  id={room.id}
-                  guest={guest}
-                  reservationStartDate={reservationStartDate}
-                  reservationEndDate={reservationEndDate}
-                  stayDuration={room.stayDuration}
-                  available={false}
-                  text="예약불가"
-                  $isWide={true}
-                />
-              </>
-            )}
-          </style.ButtonWrapper>
-        </style.Wrapper>
-
+      <style.Wrapper>
+        <style.TopWrapper>
+          <style.ReservationDate>
+            {reservationStartDate} ~ {reservationEndDate}
+          </style.ReservationDate>
+          <style.ReservationGuest> 선택 인원: {guest}인</style.ReservationGuest>
+          <style.RoomPrice>
+            {room.totalPrice.toLocaleString()}원 / {room.stayDuration}박
+          </style.RoomPrice>
+        </style.TopWrapper>
+        <style.ButtonWrapper>
+          {isRoomAvailable && isAvailableGuest ? (
+            <>
+              <CartButton
+                roomOptionId={room.id}
+                numberOfGuest={guest}
+                reservationStartDate={reservationStartDate}
+                reservationEndDate={reservationEndDate}
+                stayDuration={room.stayDuration}
+                available={true}
+              />
+              <ReservationButton
+                id={room.id}
+                guest={guest}
+                reservationStartDate={reservationStartDate}
+                reservationEndDate={reservationEndDate}
+                stayDuration={room.stayDuration}
+                available={true}
+                text="예약하기"
+                $isWide={true}
+              />
+            </>
+          ) : (
+            <>
+              <CartButton
+                roomOptionId={room.id}
+                numberOfGuest={guest}
+                reservationStartDate={reservationStartDate}
+                reservationEndDate={reservationEndDate}
+                stayDuration={room.stayDuration}
+                available={false}
+              />
+              <ReservationButton
+                id={room.id}
+                guest={guest}
+                reservationStartDate={reservationStartDate}
+                reservationEndDate={reservationEndDate}
+                stayDuration={room.stayDuration}
+                available={false}
+                text="예약불가"
+                $isWide={true}
+              />
+            </>
+          )}
+        </style.ButtonWrapper>
         {logInModal && <LoginModal onClose={() => setLogInModal(false)} />}
-      </>
+      </style.Wrapper>
     );
   }
 };
