d2e6daec90a9ec2363af2f97a40af55285a14f46
xxxjinn <tlsguswls319@naver.com>
2023-12-11 19:32:11 +0900

    Refactor: 장바구니 상품 추가 try/catch 문 없앰

--- src/feature/accommodationInformation/components/CartButton.tsx
+++ src/feature/accommodationInformation/components/CartButton.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import * as style from "../styles/cartButton";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { toastState } from "../../../recoil/toast";
@@ -6,7 +5,7 @@ import { userState } from "../../../recoil/userData";
 import { usePostCart } from "../hooks/queries/addCartData";
 import { CartButtonProps } from "../accommodationInformation.types";
 import { loginModalState } from "../recoil/accommodationLoginModal";
-import { useState } from "react";
+import { cartButtonState } from "../recoil/cartButtonDisabled";
 
 const CartButton = ({
   available,
@@ -20,34 +19,25 @@ const CartButton = ({
 
   const setToast = useSetRecoilState(toastState);
 
-  const navigation = useNavigate();
-
   const postCartMutation = usePostCart();
 
   const setLogInModal = useSetRecoilState(loginModalState);
 
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const cartButtonDisabled = useRecoilValue(cartButtonState);
 
   const handleAddCart = () => {
-    try {
-      setIsButtonDisabled(true);
-      postCartMutation.mutateAsync({
-        roomOptionId,
-        numberOfGuest,
-        reservationStartDate,
-        reservationEndDate,
-        stayDuration,
-      });
-      setToast({ open: true, message: "장바구니에 상품이 담겼습니다." });
-      setTimeout(() => setIsButtonDisabled(false), 3000);
-    } catch (e) {
-      window.alert("사용 중 문제가 발생했습니다. 메인에서 다시 시도해주세요.");
-      navigation("/");
-    }
+    postCartMutation.mutateAsync({
+      roomOptionId,
+      numberOfGuest,
+      reservationStartDate,
+      reservationEndDate,
+      stayDuration,
+    });
+    setToast({ open: true, message: "장바구니에 상품이 담겼습니다." });
   };
 
   return available ? (
-    <style.CartButton onClick={() => !isButtonDisabled && (user.accessToken ? handleAddCart() : setLogInModal(true))}>
+    <style.CartButton onClick={() => !cartButtonDisabled && (user.accessToken ? handleAddCart() : setLogInModal(true))}>
       <style.CartIcon />
     </style.CartButton>
   ) : (
--- src/feature/accommodationInformation/hooks/queries/addCartData.ts
+++ src/feature/accommodationInformation/hooks/queries/addCartData.ts
@@ -1,16 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postCart } from "../../api";
 import { PostCart } from "../../accommodationInformation.types";
+import { useSetRecoilState } from "recoil";
+import { cartButtonState } from "../../recoil/cartButtonDisabled";
 
 export const usePostCart = () => {
   const queryClient = useQueryClient();
+
+  const navigation = useNavigate();
+
+  const setIsButtonDisabled = useSetRecoilState(cartButtonState);
 
   return useMutation({
     mutationFn: ({ roomOptionId, numberOfGuest, reservationStartDate, reservationEndDate, stayDuration }: PostCart) => {
       return postCart(roomOptionId, numberOfGuest, reservationStartDate, reservationEndDate, stayDuration);
     },
     onSuccess: () => {
+      setIsButtonDisabled(true);
       queryClient.invalidateQueries({ queryKey: ["fetchCarts"] });
+      setTimeout(() => setIsButtonDisabled(false), 3000);
+    },
+    onError: () => {
+      window.alert("장바구니에 상품을 추가하는 과정에서 문제가 발생했습니다. 메인화면으로 돌아갑니다.");
+      navigation("/");
     },
   });
 };
