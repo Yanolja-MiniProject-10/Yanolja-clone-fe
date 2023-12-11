a2d9006a2f8ec3a08359c039f195fe86d845bc22
xxxjinn <tlsguswls319@naver.com>
2023-12-11 19:43:42 +0900

    Refactor: postReservationInstant 함수 try/catch문 삭제

--- src/feature/accommodationInformation/components/ReservationButton.tsx
+++ src/feature/accommodationInformation/components/ReservationButton.tsx
@@ -2,7 +2,6 @@ import { useRecoilValue, useSetRecoilState } from "recoil";
 import * as style from "../styles/reservationButton";
 import { userState } from "../../../recoil/userData";
 import { loginModalState } from "../recoil/accommodationLoginModal";
-import { useNavigate } from "react-router-dom";
 import { ReservationButtonProps } from "../accommodationInformation.types";
 import { usePostSingleReservation } from "../hooks/queries/addSingleReservationData";
 
@@ -19,32 +18,16 @@ const ReservationButton = ({
   const user = useRecoilValue(userState);
   const setLogInModal = useSetRecoilState(loginModalState);
 
-  const navigation = useNavigate();
-
   const postSingleReservationMutation = usePostSingleReservation();
 
-  const postReservationInstant = async () => {
-    try {
-      const data = await postSingleReservationMutation.mutateAsync({
-        roomOptionId: id,
-        numberOfGuest: guest,
-        reservationStartDate,
-        reservationEndDate,
-        stayDuration,
-      });
-      const cartId = data.data.cartId;
-      const cartProducts = [data.data.accommodations[0].roomOptions[0].cartProductId];
-
-      navigation("/reservation", {
-        state: {
-          cartId,
-          cartProducts,
-        },
-      });
-    } catch (e) {
-      window.alert("사용 중 문제가 발생했습니다. 메인에서 다시 시도해주세요.");
-      navigation("/");
-    }
+  const postReservationInstant = () => {
+    postSingleReservationMutation.mutateAsync({
+      roomOptionId: id,
+      numberOfGuest: guest,
+      reservationStartDate,
+      reservationEndDate,
+      stayDuration,
+    });
   };
 
   return available ? (
