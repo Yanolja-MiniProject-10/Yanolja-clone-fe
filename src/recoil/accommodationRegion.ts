6e076accd9dd8a4d2d14f7fa9c4f2c3deeaac166
applevalley <skatkd7571@naver.com>
2023-12-13 13:27:19 +0900

    Refactor: recoil atom import 주소 변경

--- src/recoil/accommodationRegion.ts
+++ src/recoil/accommodationRegion.ts
@@ -1,5 +1,5 @@
 import { atom } from "recoil";
-import { AccommodationRegionDefault } from "../../feature/accommodation/accommodation.types";
+import { AccommodationRegionDefault } from "../feature/accommodation/accommodation.types";
 import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
